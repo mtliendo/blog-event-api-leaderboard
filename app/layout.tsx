6b2403c681c98907a31eb132dcdd122faf695522
mtliendo <mtliendo@focusotter.com>
2024-10-29 16:31:43 -0500

    update with correct deps

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
-
-import '@aws-amplify/ui-react/styles.css'
 import ConfigureAmplifyClientSide from '@/components/configureAmplify'
 
 const geistSans = localFont({
